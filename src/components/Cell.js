0668a0b80195c2c6888986bcb77b801523b71b57
Lisa Herzberg <lisamarieherzberg@gmail.com>
2019-10-11 20:51:29 -0700

    updated styles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,20 +5,11 @@ export const Cell = (props) => {
     const {value, onClick, isPeer, rowPeer, columnPeer, squarePeer, isSelected, sameValue, prefilled, notes, conflict} = props;
 
     const renderBackgroundColor = () => {
-        if(conflict && !prefilled){
-            if(conflict && isPeer && sameValue) return {backgroundColor: 'rgba(245, 3, 0, 0.73)', color: 'red'};
-            if(sameValue) return {backgroundColor: 'rgba(113, 73, 245, 0.58)', color: 'red'};
-            if(isSelected) return {backgroundColor: "rgba(248, 211, 74, 0.96)", color: 'red'};
-            if(squarePeer) return {backgroundColor: "rgba(248, 219, 78, 0.57)", color: 'red'};
-            if(rowPeer || columnPeer) return {backgroundColor: "rgba(95, 212, 246, 0.78)",color: 'red'};
-            if(conflict && !prefilled) return {color: 'red'};
-        } else {
             if(conflict && isPeer && sameValue) return {backgroundColor: 'rgba(245, 3, 0, 0.73)'};
             if(sameValue) return {backgroundColor: 'rgba(113, 73, 245, 0.58)'};
             if(isSelected) return {backgroundColor: "rgba(248, 211, 74, 0.96)"};
             if(squarePeer) return {backgroundColor: "rgba(248, 219, 78, 0.57)"};
             if(rowPeer || columnPeer) return {backgroundColor: "rgba(95, 212, 246, 0.78)"};
-        }
     };
 
 
